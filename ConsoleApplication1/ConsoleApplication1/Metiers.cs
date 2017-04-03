89f9685d4a1cf33d6ee44518ca9789ea01b42b60
glavender <glavender@GLAVENDER17-DE>
2017-04-03 10:03:31 +0200

    maj

---
+++ ConsoleApplication1/ConsoleApplication1/Metiers.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+
+    [Flags]
+
+    public enum Activités
+    {
+        Aucune = 0,
+        DBE = 1,
+        ARF = 2,
+        ANF = 4,
+        DES = 8,
+        INF = 16,
+        ART = 32,
+        ANT = 64,
+        DEV = 128,
+        RTP = 256,
+        TES = 512,
+        GDP = 1024
+    }
+    public enum LibelléActivités
+    {
+        Aucune = 0,
+        DéfinitionDesBesoins = 1,
+        ArchitectureFonctionnelle = 2,
+        AnalyseFonctionnelle = 4,
+        Design = 8,
+        Infographie = 16,
+        ArchitectureTechnique = 32,
+        AnalyseTechnique = 64,
+        Développement = 128,
+        RédactionDePlanDeTest = 256,
+        Test = 512,
+        GestionDeProjet = 1024
+    }
+
+    public enum CodeMetiers
+    {
+        ANA, CDP, DEV, DES, TES
+    }
+    public enum LibelléMetiers
+    {
+        Analyste, ChefDeProjet, Développeur, Designer, Testeur
+    }
+
+
+
+    public class Metiers
+    {
+        public Activités Activité { get; set; }
+        public LibelléActivités LibelléActivité { get; set; }
+        public CodeMetiers CodeMetier { get; set; }
+        public LibelléMetiers LibelléMetier { get; set; }
+
+    }
+}
