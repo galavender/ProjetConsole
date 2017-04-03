64a909d4006c2beac935ce56a33902e441f9aac6
bguillaumin <bguillaumin@BGUILLAUMI17-DE>
2017-04-03 10:10:06 +0200

    Class dal

---
+++ ConsoleApplication1/ConsoleApplication1/DAL.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    public class DAL
+    {
+        #region Propriété
+        public Dictionary<int, object> ListeMétier { get; set; }
+        public Dictionary<int, object> ListePersonne { get; set; }
+        public Dictionary<int, object> ListeTache { get; set; }
+        public object Logiciel { get; set; }
+        #endregion
+
+        #region Méthode
+
+        #endregion
+
+    }
+}
---
+++ ConsoleApplication1/ConsoleApplication1/Taches.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    public class Taches
+    {
+
+    }
+
+    public class TachesProd : Taches
+    {
+        #region Propriétés
+        public DateTime DateDébut { get; set; }
+        public int DuréeRéalisée { get; set; }
+        public int DuréeRestante { get; set; }
+        public int DuréePrévue { get; }
+        #endregion
+    }
+
+    public class TachesAnne : Taches
+    {
+        #region Propriétés
+        public int ActivitéAnne { get; }
+        public int CumulTempsPassé { get; }
+        #endregion
+    }
+}
