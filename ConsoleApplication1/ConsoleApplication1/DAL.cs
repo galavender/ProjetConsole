32f2085e0cd7b230ca606d6ee5e428e5afd73919
bguillaumin <bguillaumin@BGUILLAUMI17-DE>
2017-04-03 11:32:11 +0200

    Méthode DAL

--- ConsoleApplication1/ConsoleApplication1/DAL.cs
+++ ConsoleApplication1/ConsoleApplication1/DAL.cs
@@ -3,21 +3,69 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace ConsoleApplication1
 {
     public class DAL
     {
         #region Propriété
-        public Dictionary<int, object> ListeMétier { get; set; }
-        public Dictionary<int, object> ListePersonne { get; set; }
-        public Dictionary<int, object> ListeTache { get; set; }
-        public object Logiciel { get; set; }
+        public Dictionary<int, Metiers> ListeMétier { get; set; }
+        public Dictionary<int, Personnes> ListePersonne { get; set; }
+        public Dictionary<int, Taches> ListeTache { get; set; }
+        public object Logiciel { get; set; }    //A modifier avec la classe logiciel
         #endregion
 
         #region Méthode
-
+        public void ChargerDonnées()
+        {
+            string chemin = @"..\..\Données.xlsx";  
+            int compteur = 0;
+            using (StreamReader str = new StreamReader(chemin)) 
+            {
+                string ligne;
+                while ((ligne = str.ReadLine()) != null)
+                {
+                    compteur++;
+                    if (compteur < 2) continue;
+                    var tab = ligne.Split('\t');
+                    try
+                    {
+                        var DonnéesGestionTache = new DonnéesGestionTaches                   
+                        {
+                            NumTache = int.Parse(tab[0]),                                           
+                            Version = tab[1],   //A modifier                            
+                            Personne = new Personnes() { Code = tab[2]},                        //Nouvelle instance de Personne renseignant le champ Code
+                            CodeActivité = (Activités) Enum.Parse(typeof(Activités), tab[3]),
+                            Tache = new TachesProd()                                            //Nouvelle instance de Tache renseignant tous ces champs
+                            {
+                                Libellé = tab[4],
+                                DateDébut = DateTime.Parse(tab[5]),
+                                DuréePrévue = int.Parse(tab[6]),
+                                DuréeRéalisée = int.Parse(tab[7]),
+                                DuréeRestante = int.Parse(tab[8])
+                            }
+                        };
+                    }
+                    catch (FormatException)
+                    {
+                        throw new FormatException();
+                    }
+                }
+            }
+        }
         #endregion
 
     }
+
+    public class DonnéesGestionTaches
+    {
+        public int NumTache { get; set; }
+        public Object Version { get; set; } //A modifier avec la classe Version
+        public Personnes Personne { get; set; }
+        public Activités CodeActivité { get; set; }
+        public Taches Tache { get; set; }
+
+    }
+
 }
--- ConsoleApplication1/ConsoleApplication1/Taches.cs
+++ ConsoleApplication1/ConsoleApplication1/Taches.cs
@@ -8,7 +8,7 @@ namespace ConsoleApplication1
 {
     public class Taches
     {
-
+        public string Libellé { get; set; }
     }
 
     public class TachesProd : Taches
@@ -17,15 +17,15 @@ namespace ConsoleApplication1
         public DateTime DateDébut { get; set; }
         public int DuréeRéalisée { get; set; }
         public int DuréeRestante { get; set; }
-        public int DuréePrévue { get; }
+        public int DuréePrévue { get; set; }
         #endregion
     }
 
     public class TachesAnne : Taches
     {
         #region Propriétés
-        public int ActivitéAnne { get; }
-        public int CumulTempsPassé { get; }
+        public int ActivitéAnne { get; set; }
+        public int CumulTempsPassé { get; set; }
         #endregion
     }
 }
